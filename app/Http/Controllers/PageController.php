2c22a3ea3c068fff176bbac2071ab5d2e6c226a4
Hyperlink '18 <47172895+ifetayo14@users.noreply.github.com>
2021-07-15 00:22:30 +0700

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,30 +7,20 @@ use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller
 {
-    public function toAdmin(){
-        $periode = DB::table('periode')->where(['status' => 'Aktif'])->get(); 
-        //artikel
-        $artikel = DB::table('artikel')->where(['periode_id' => $periode[0]->periode_id])->get(); 
-        //berita
-        $berita = DB::table('berita')->where(['periode_id' => $periode[0]->periode_id])->get(); 
-        //kotbah
-        $kotbah = DB::table('kotbah')->where(['periode_id' => $periode[0]->periode_id])->get(); 
-        return view('admin.index', compact(['periode', 'artikel', 'berita', 'kotbah']));
-    }
-
-    public function toJemaat(){
-        return view('jemaat.index');
-    }
-
-    public function toPendeta(){
-        return view('pendeta.index');
-    }
-
-    public function toSekjen(){
-        return view('sekjen.index');
-    }
-
-    public function toTimMajalah(){
-        return view('timMajalah.index');
+    public function dashboard()
+    {
+        if (Session::get('user_id') == '1') {
+            $periode = DB::table('periode')->where(['status' => 'Aktif'])->get();
+            //artikel
+            $artikel = DB::table('artikel')->where(['periode_id' => $periode[0]->periode_id])->get();
+            //berita
+            $berita = DB::table('berita')->where(['periode_id' => $periode[0]->periode_id])->get();
+            //kotbah
+            $kotbah = DB::table('kotbah')->where(['periode_id' => $periode[0]->periode_id])->get();
+            return view('index', compact(['periode', 'artikel', 'berita', 'kotbah']));
+        }
+        else{
+            return view('index');
+        }
     }
 }
