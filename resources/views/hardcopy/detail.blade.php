5b23de7733cd3c8885fc1a24dd5e12b31644b6be
Johannes Manurung <jmanurung753@gmail.com>
2021-12-24 02:05:40 +0700

    detail begin

--- resources/views/hardcopy/detail.blade.php
+++ resources/views/hardcopy/detail.blade.php
@@ -4,16 +4,50 @@
     Detail HardCopy
 @endsection
 @section('main-content')
-    <div class="container card mb-4" style="min-height: 600px;">
+    <div class="container card mb-4 p-1" style="min-height: 400px;">
 
-        <div class="row mt-4">
+        <div class="row p-3">
             <div class="col-md-5">
-                <img class="w-100" src="https://winstarlink.com/wp-content/uploads/2020/02/jasa-pembuatan-desain-katalog-Portrait-10.jpg" alt="" height="450px">
+                <img class="w-100" src="https://winstarlink.com/wp-content/uploads/2020/02/jasa-pembuatan-desain-katalog-Portrait-10.jpg" alt="" height="400px">
             </div>
             <div class="col-md-7">
                 <h1 class="page-head-title h3 text-center radius">Majalah bulan agustus 2001</h1>
+                <div class="card-text mt-4 pt-2 pb-2 pl-2 pr-2" style="min-height: 300px">
+                    <div class="row  mt-1">
+                        <div class="col-sm-2">Deskripsi</div>
+                        <div class="col-sm-0">:</div>
+                        <div class="col-sm-9">Majalah hardcopy untuk bulan agustus 2021  </div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-sm-2">Berat</div>
+                        <div class="col-sm-0">:</div>
+                        <div class="col-sm-9">0,5 ONS</div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-sm-2">Periode</div>
+                        <div class="col-sm-0">:</div>
+                        <div class="col-sm-9">Desember 2021</div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-sm-2">Stok</div>
+                        <div class="col-sm-0">:</div>
+                        <div class="col-sm-9">2</div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-sm-2">Harga</div>
+                        <div class="col-sm-0">:</div>
+                        <div class="col-sm-9">Rp. 45.000</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <a class="btn btn-success w-100" href="" >Beli</a>
+                </div>
+
+
             </div>
+
         </div>
+
 
 
 
