72092a9fa854b2eeb9dfa3f4fb93e7c67ca56a9d
Johannes Manurung <jmanurung753@gmail.com>
2021-12-25 00:44:50 +0700

    hardcopy jemaat

--- resources/views/hardcopy/detail.blade.php
+++ resources/views/hardcopy/detail.blade.php
@@ -1,42 +1,51 @@
 @extends('layout.layout')
 
 @section('title')
-    Detail HardCopy
+    Detail HardCopy {{$dataHardCopy->nama}}
 @endsection
 @section('main-content')
-    <div class="container card mb-4 p-1" style="min-height: 400px;">
+    <div class="container card mb-4 p-2 align-self-center" style="min-height: 400px;">
 
         <div class="row p-3">
             <div class="col-md-5">
                 <img class="w-100" src="https://winstarlink.com/wp-content/uploads/2020/02/jasa-pembuatan-desain-katalog-Portrait-10.jpg" alt="" height="400px">
             </div>
             <div class="col-md-7">
-                <h1 class="page-head-title h3 text-center radius">Majalah bulan agustus 2001</h1>
+                <h1 class="page-head-title h3 text-center radius">{{$dataHardCopy->nama}}</h1>
                 <div class="card-text mt-4 pt-2 pb-2 pl-2 pr-2" style="min-height: 300px">
                     <div class="row  mt-1">
                         <div class="col-sm-2">Deskripsi</div>
                         <div class="col-sm-0">:</div>
-                        <div class="col-sm-9">Majalah hardcopy untuk bulan agustus 2021  </div>
+                        <div class="col-sm-9">{{$dataHardCopy->deskripsi}}</div>
                     </div>
                     <div class="row mt-1">
                         <div class="col-sm-2">Berat</div>
                         <div class="col-sm-0">:</div>
-                        <div class="col-sm-9">0,5 ONS</div>
+                        <div class="col-sm-9">{{$dataHardCopy->berat}} kg</div>
                     </div>
                     <div class="row mt-1">
                         <div class="col-sm-2">Periode</div>
                         <div class="col-sm-0">:</div>
-                        <div class="col-sm-9">Desember 2021</div>
+                        <div class="col-sm-9">{{$dataHardCopy->bulan . ' ' .$dataHardCopy->tahun }}</div>
                     </div>
                     <div class="row mt-1">
                         <div class="col-sm-2">Stok</div>
                         <div class="col-sm-0">:</div>
-                        <div class="col-sm-9">2</div>
+                        <div class="col-sm-9">{{$dataHardCopy->stok}}</div>
                     </div>
                     <div class="row mt-1">
                         <div class="col-sm-2">Harga</div>
                         <div class="col-sm-0">:</div>
-                        <div class="col-sm-9">Rp. 45.000</div>
+                        <div class="col-sm-9">Rp. {{$dataHardCopy->harga}}</div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-sm-2 align-self-center">
+                            Jumlah
+                        </div>
+                        <div class="col-sm-0 align-self-center">:</div>
+                        <div class="col-sm-2">
+                            <input type="number" class="form-control" id="stok">
+                        </div>
                     </div>
                 </div>
                 <div class="row">
@@ -52,4 +61,7 @@
 
 
     </div>
+    <script>
+        document.getElementById()t
+    </script>
 @endsection
